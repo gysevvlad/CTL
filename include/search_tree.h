430ee65b9daf5ac2277b959b63b3109838d72797
gysevvlad <gysevvlad@gmail.com>
2015-01-24 21:49:51 +0300

    add function search_tree_add

--- include/search_tree.h
+++ include/search_tree.h
@@ -54,8 +54,73 @@ search_tree_node_new_##key_t##_##data_t( ){\
 	return (search_tree_node_##key_t##_##data_t*) malloc ( \
 		sizeof( search_tree_node_##key_t##_##data_t ) );\
 }\
-
-
+/*_____________________________________________________________________________
+	init search_tree_node
+*/\
+void search_tree_node_init_##key_t##_##data_t(\
+	search_tree_node_##key_t##_##data_t *tree, \
+	key_t key, data_t data, \
+	search_tree_node_##key_t##_##data_t* parent ) {\
+	tree-> key = key;\
+	tree-> data = data;\
+	tree-> parent = parent;\
+	tree-> left = NULL;\
+	tree-> right = NULL;\
+}\
+\
+/*_____________________________________________________________________________
+	init search_tree_node
+*/\
+search_tree_node_##key_t##_##data_t* \
+search_tree_node_create_##key_t##_##data_t( key_t key, data_t data, \
+	search_tree_node_##key_t##_##data_t* parent ) {\
+	search_tree_node_##key_t##_##data_t* tree;\
+	tree = search_tree_node_new_##key_t##_##data_t( );\
+	search_tree_node_init_##key_t##_##data_t( tree, key, data, parent );\
+	return tree;\
+}\
+\
+/*_____________________________________________________________________________
+	add <key_t, data_t> to search_tree
+*/\
+void search_tree_add_##key_t##_##data_t( search_tree_##key_t##_##data_t *tree,\
+	key_t key, data_t data ) {\
+	search_tree_node_##key_t##_##data_t *root = tree-> root;\
+	int cmp;\
+	if ( root == NULL ) {\
+		tree-> root = search_tree_node_create_##key_t##_##data_t(\
+			key, data, NULL );\
+		return;\
+	} \
+next:\
+	cmp = tree-> cmp( &key, &root-> key );\
+	if ( cmp < 0 ) {\
+		if ( root-> left == NULL ) {\
+			root-> left = search_tree_node_create_##key_t##_##data_t(\
+				key, data, root );\
+			return;\
+		} else {\
+			root = root-> left;\
+			goto next;\
+		}\
+	}\
+	if ( cmp > 0 ) {\
+		if ( root-> right == NULL ) {\
+			root-> right = search_tree_node_create_##key_t##_##data_t( \
+				key, data, root );\
+			return;\
+		} else {\
+			root = root-> right;\
+			goto next;\
+		}\
+	}\
+	if ( cmp == 0 ) {\
+		root-> data = data;\
+		root-> key = key;\
+		return;\
+	}\
+}\
+\
 
 
 #endif
--- test/search_tree_test.c
+++ test/search_tree_test.c
@@ -1,29 +1,51 @@
 #include "search_tree.h"
+#include "test.h"
 
 int int_compare( int* a, int *b ) {
-	if ( a < b ) return -1;
-	if ( a = b ) return 0;
+	int af = *a, bf = *b;
+	if ( af < bf ) return -1;
+	if ( af == bf ) return 0;
 	return 1;	
 }
 
-int user_compare( int* a, int *b ) {
-	return 1;
-}
+DECLARE_SEARCH_TREE( int, double )
 
-DECLARE_SEARCH_TREE( int, int )
+int add_test();
 
 main() {
-	#define search_tree_t search_tree_int_int
-	#define search_tree_init_user search_tree_init_user_int_int 
-	#define search_tree_init_default search_tree_init_default_int_int 
-	#define search_tree_node_t search_tree_node_int_int 
-	#define search_tree_node_new search_tree_node_new_int_int
+	test( add );
+}
 
-	search_tree_node_t *node_ptr;
-	search_tree_t tree1, tree2;
+int add_test() {
+	search_tree_int_double tree;
 
-	search_tree_init_default( &tree1 );
-	search_tree_init_user( &tree2, user_compare );
-	node_ptr = search_tree_node_new( );
+	search_tree_init_user_int_double( &tree, int_compare );
 
+	search_tree_add_int_double( &tree, 12, 1.2 );
+	checkNoNull( tree.root );
+	checkNull( tree.root-> left );
+	checkNull( tree.root-> right );
+	checkEq( tree.root-> key, 12 );
+	checkEq( tree.root-> data, 1.2 );
+
+	search_tree_add_int_double( &tree, 8, 4.5 );
+	checkNoNull( tree.root-> left );
+	checkNull( tree.root-> right );
+	checkNull( tree.root-> left-> left );
+	checkNull( tree.root-> left-> right );
+	checkEq( tree.root, tree.root-> left-> parent );
+	checkEq( tree.root-> left-> key, 8 );
+
+	search_tree_add_int_double( &tree, 6, 9.3 );
+	checkEq( tree.root-> left-> left-> data, 9.3 );
+
+	search_tree_add_int_double( &tree, 16, 10.3 );
+	checkEq( tree.root-> right-> data, 10.3 );
+	checkEq( tree.root-> right-> key, 16 );
+	checkEq( tree.root-> right-> parent, tree.root );
+
+	search_tree_add_int_double( &tree, 9, 9.3 );
+	checkEq( tree.root-> left-> right-> key, 9 );
+
+	return 1;
 }
